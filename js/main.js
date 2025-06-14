6a29fe5dd63218649f3d6947cd8c1345855c4975
Manik Gupta <manikgupta254@gmail.com>
2025-06-14 15:57:05 +0530

    Added header & hero section

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ window.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => loader.remove(), 100);
    }, 1000);
 });
+
+// Header Background Change After Scroll
+window.addEventListener("scroll", function () {
+   const header = document.getElementById("mainHeader");
+   if (window.scrollY > 70) {
+      header.classList.add("scrolled");
+   } else {
+      header.classList.remove("scrolled");
+   }
+});
